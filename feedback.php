690712d98d74727cd14cc7e21d5933c350c5a967
Harsh2432 <jadhavharsh243@gmail.com>
2022-06-28 21:13:16 +0530

    Update feedback.php

---
+++ feedback.php
@@ -0,0 +1,28 @@
+<?php include 'inc/header.php'; ?>
+
+<?php
+    $sql = 'SELECT * FROM feedback';
+    $result = mysqli_query($conn, $sql);
+    $feedback = mysqli_fetch_all($result, MYSQLI_ASSOC);
+?>
+
+<h2>Past Feedback</h2>
+
+<?php if (empty($feedback)) : ?>
+    <p class="lead mt3">There is no feedback</p>
+<?php endif; ?>
+
+<?php foreach ($feedback as $item) : ?>
+    <div class="card my-3 w-75">
+        <div class="card-body text-center">
+            <?php echo $item['body']; ?>
+            <div class="bg-secondary p-2 text-white mt-2">
+                By <?php echo $item['name']; ?>
+                <br>
+                On <?php echo $item['date']; ?>
+            </div>
+        </div>
+    </div>
+<?php endforeach; ?>
+
+<?php include 'inc/footer.php'; ?>
